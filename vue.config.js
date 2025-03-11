acfe3eaad48cd7a111a09bcc99464bb90d7e769d
HuangJiaLong <jialong.huang01@hand-china.com>
2025-03-11 20:15:06 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: './'
+  publicPath: '/ToDoList/',
+  outputDir: 'docs'
 })
